58c5eb6f888a35984e6c72ef88efe0bdfbf06c7f
yono.ap <yono.ap@icloud.com>
2023-11-05 07:21:46 +0900

    Initial Commit

---
+++ SwiftUIDelegate/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUIDelegate
+//
+//  Created by no name on 2023/11/05.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIDelegate/SwiftUIDelegateApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIDelegateApp.swift
+//  SwiftUIDelegate
+//
+//  Created by no name on 2023/11/05.
+//  
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDelegateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
