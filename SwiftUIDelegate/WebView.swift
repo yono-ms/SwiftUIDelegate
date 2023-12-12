311d106257645871a45d59d1e4062c1054a4b660
yono.ap <yono.ap@icloud.com>
2023-12-13 07:47:15 +0900

    add WebViewCoordinator.

--- SwiftUIDelegate/WebView.swift
+++ SwiftUIDelegate/WebView.swift
@@ -10,40 +10,44 @@ import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable {
+
     @Binding var isLoading: Bool
     
-    func makeUIView(context: Context) -> some UIView {
-        let url = URL(string: "https://google.com")
-        let request = URLRequest(url: url!)
+    let script = """
+        NativeApp = {}
+        NativeApp.request = function (message) {
+            window.webkit.messageHandlers.request.postMessage(message)
+        }
+    """
+
+    func makeUIView(context: Context) -> WKWebView {
+        print("WebView makeUIView")
+        
+        let path = Bundle.main.path(forResource: "sample", ofType: "html") ?? ""
+        let url = URL(fileURLWithPath: path)
+        let request = URLRequest(url: url)
         let webView = WKWebView()
         webView.navigationDelegate = context.coordinator
+        webView.uiDelegate = context.coordinator
+        webView.configuration.userContentController.add(context.coordinator, name: "request")
+        webView.configuration.userContentController.addUserScript(WKUserScript(source: script, injectionTime: .atDocumentStart, forMainFrameOnly: true))
+        context.coordinator.setWKWebView(webView: webView)
         webView.load(request)
         return webView
     }
     
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        print("WebView updateUIView")
+    }
+    
     func makeCoordinator() -> WebViewCoordinator {
+        print("WebView makeCoordinator")
         return WebViewCoordinator(owner: self)
     }
     
-    func updateUIView(_ uiView: UIViewType, context: Context) {
-        
-    }
-}
-
-class WebViewCoordinator: NSObject, WKNavigationDelegate {
-    private let owner: WebView
-    init(owner: WebView) {
-        self.owner = owner
-        super.init()
-    }
+    typealias UIViewType = WKWebView
+    typealias Coordinator = WebViewCoordinator
     
-    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        owner.isLoading = true
-    }
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        owner.isLoading = false
-    }
 }
 
 #Preview {
